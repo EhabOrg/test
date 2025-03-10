f97c896acc69e37c8af97d8a1255286f4efc8a35
EhabSilawi <86913871+EhabSilawi@users.noreply.github.com>
2025-03-10 14:52:43 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "data" {
   # bucket is public
   # bucket is not encrypted
-  # bucket does not have access logsssdfaf
+  # bucket does not have access logsssdfafsadsdsdsa
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
   force_destroy = true
